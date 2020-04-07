b50dd038c98b05cca487149e475b0529ffeb937f
tessavdheiden <tessavdheiden@gmail.com>
2020-04-07 12:59:51 +0200

    added all

--- tests/news_finder_test.py
+++ tests/news_finder_test.py
@@ -8,5 +8,10 @@ class NewsFinderTest(unittest.TestCase):
         newsfinder = NewsFinder("fakeurl")
         self.assertIsInstance(newsfinder.news_pages(), list)
 
+    def test_get_1_page(self):
+        newsfinder = NewsFinder("http://127.0.0.1:8080")
+        self.assertEqual(len(newsfinder.news_pages()), 1)
+
+
 if __name__ == "__main__":
     unittest.main()
--- tests/news_parser_test.py
+++ tests/news_parser_test.py
@@ -6,10 +6,11 @@ from news import News
 
 class NewsParserTest(unittest.TestCase):
     def test_parse(self):
-        page = "fakeurl"
+        page = "http://127.0.0.1:8080/page0" # TODO: catch "fakeurl"
         newsparser = NewsParser(page)
         news = newsparser.parse()
         self.assertIsInstance(news, News)
+
 
 if __name__ == "__main__":
     unittest.main()
