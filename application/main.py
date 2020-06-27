b70ef9d2f3daac6a1e7866850881414e980de7f9
tessavdheiden <tessavdheiden@gmail.com>
2020-06-27 10:53:07 +0200

    added some error handling

--- application/main.py
+++ application/main.py
@@ -4,25 +4,29 @@ from src.summarize import generate_summary
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 document_list = []
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    rawtext = request.form['rawtext']
-    summary = generate_summary(rawtext)
+
+@app.route('/api/input', methods=['GET', 'POST'])
+def summarize_text():
+    json_data = request.get_json()
+    rawtext = json_data['text']
+
+    try:
+        summary = generate_summary(rawtext)
+    except Exception as exp:
+        print("Exception: ", exp)
+        summary = ""
+
     document = Document(id=len(document_list), text=rawtext, summary=summary)
     document_list.append(document)
-    return render_template('index.html', items=document_list, result=len(document_list))
 
+    if summary == "":
+        status = 'error'
+    else:
+        status = 'success'
+    return jsonify(text=rawtext, summary=summary, status=status)
 
-@app.route('/results',methods=['POST'])
-def results():
-    data = request.get_json(force=True)
-    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
