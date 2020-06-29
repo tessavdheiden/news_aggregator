4c1f4eb26eaf0e2af7d41de9d7cc1408316a92ce
tessavdheiden <tessavdheiden@gmail.com>
2020-06-29 09:06:09 +0200

    added logging details for document

--- src/document.py
+++ src/document.py
@@ -1,5 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(filename='document.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
 class Document(object):
     def __init__(self, id: int, text: str, summary: str):
         self.id = id
         self.text = text
         self.summary = summary
+
+        logger.info(f'Created document {self.id}')
