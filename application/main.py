d0391ac3efa1c7dba032e94e7428dce9d2e18100
tessavdheiden <tessavdheiden@gmail.com>
2020-06-30 09:02:25 +0200

    added logging of exceptions

--- application/main.py
+++ application/main.py
@@ -7,6 +7,18 @@ from src.document import Document
 from src.summarize import generate_summary
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('sample.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
 app = Flask(__name__)
 
 document_list = []
@@ -23,8 +35,10 @@ def summarize_text():
         document_list.append(document)
         return jsonify(text=rawtext, summary=summary)
     except IndexError as e:
+        logger.error('Exception: Text too short')
         return jsonify(error=f"Text too short: {e}"), status.HTTP_411_LENGTH_REQUIRED
     except Exception as e:
+        logger.error('Exception: Server error')
         return jsonify(error=f"Unable to generate summary: {e}"), status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
